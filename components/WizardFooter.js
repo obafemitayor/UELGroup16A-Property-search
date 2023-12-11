d28a7b60ccfc3497d1319f02fbb9cab1e56cd0d8
Omotayo Obafemi <obafemitayor@gmail.com>
2023-12-11 23:08:41 +0000

    Removed redundant comments

--- components/WizardFooter.js
+++ components/WizardFooter.js
@@ -3,42 +3,6 @@ import * as React from "react";
 import { useWizard } from "../lib/react-use-wizard";
 import { Button, Box, Center } from "@chakra-ui/react";
 
-// const Button = styled.button`
-//   border: 1px solid red;
-//   margin-right: 2rem;
-//   padding: 0.5rem;
-//   border-radius: 5px;
-//   cursor: pointer;
-// `
-
-// export const Actions = styled('div')`
-//   display: flex;
-//   justify-content: center;
-//   margin: 1rem 0;
-//   gap: 1rem;
-//   flex-direction: row;
-// `;
-
-// export const Info = styled('div')`
-//   display: flex;
-//   justify-content: center;
-//   flex-direction: column;
-//   gap: 0;
-
-//   & > p {
-//     margin: 0.25rem 0;
-//   }
-
-//   @media screen and (min-width: 768px) {
-//     flex-direction: row;
-//     gap: 1rem;
-
-//     & > p {
-//       margin: initial;
-//     }
-//   }
-// `;
-
 const WizardFooter = () => {
   const {
     nextStep,
